140513629465e111869fde37a477ee13a2a889f7
Alexis Narvaez <alexisnarvaez97@hotmail.com>
2019-10-11 12:02:40 -0500

    Actualizar avatar del usuario

--- src/app/components/avatar-selector/avatar-selector.component.ts
+++ src/app/components/avatar-selector/avatar-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 
 @Component({
   selector: "app-avatar-selector",
@@ -9,6 +9,7 @@ export class AvatarSelectorComponent implements OnInit {
 
   
   @Output() avatarSelec = new EventEmitter<string>();
+  @Input() avatarActual = 'av-1.png';
 
   avatarSlide = {
     slidesPerView: 3.5
@@ -51,7 +52,19 @@ export class AvatarSelectorComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.avatars.forEach( avatar => avatar.seleccionado = false);
+
+    for (const avatar of this.avatars) {
+
+      if(avatar.img === this.avatarActual) {
+        avatar.seleccionado = true;
+        break;
+      }
+    }
+
+  }
 
   seleccionarAvatar(avatar) {
     this.avatars.forEach(av => (av.seleccionado = false));
